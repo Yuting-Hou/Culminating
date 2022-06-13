9a97557ef6468b16e7252ee8ce700de0a25592d4
yuting <yuting.yh@gmail.com>
2022-06-12 23:32:02 -0400

    init commit

---
+++ fabric-example-mod-1.18/src/main/java/net/fabricmc/example/Example.java
@@ -0,0 +1,37 @@
+/**
+ * Example.java
+ * This is the "main" class for my fabric mod
+ */
+package net.fabricmc.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.example.block.ModBlocks;
+import net.fabricmc.example.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Example implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(Example.mod_id);
+
+	//mod id of this mod
+	public static final String mod_id = "modid";
+
+	@Override
+	/**
+	 * This runs when minecraft starts
+	 * Register all mod content here
+	 * Register is the process of taking mod items and making it known to minecraft
+	 */
+	public void onInitialize() {
+
+		//tells Fabric to registers mod items
+		ModItems.registerModItems();
+
+		ModBlocks.registerModBlocks();
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
